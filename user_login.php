6f4148a6e7526b2893c5a5edfc8548820129031d
pkinsella32 <pkinsella32@users.noreply.github.com>
2017-11-08 21:41:29 +0000

    working version fixed issues from last push

--- user_login.php
+++ user_login.php
@@ -25,7 +25,7 @@ include 'header.php';
        <!-- <center> <img  src="img/logo.png" alt="Logo" max-width="100%" ></center>-->
           
          
-                        <div >
+                        <div align="center" >
                        
                      
                             <?php
@@ -39,25 +39,17 @@ include 'header.php';
                          } 
                          
                          else{
-                                   // echo"<h1>Candidate Login</h1>";
-                            //  echo" <form action='includes/login.inc.php' method='POST'>
-                             //   <input type ='text' name='uid' placeholder='Username' required>
-                             //   <input type ='password' name='pwd' placeholder='Password' required>
-                             //   <button type ='submit'>Login</button>
-                             //   </form>";
+                                    echo"<h1>Candidate Login</h1>";
+                             echo" <form action='includes/login.inc.php' method='POST'>
+                              <input type ='text' name='uid' placeholder='Username' required>
+                               <input type ='password' name='pwd' placeholder='Password' required>
+                               <button type ='submit'>Login</button>
+                               </form>";
 
-                                echo"<div class='login-page'>
-                                <div class='form'>
-                                <h1>Candidate Login</h1>
+                             
                                 
-                                  <form action='includes/login.inc.php' method='POST' class='login-form'>
-                                    <input type='text' name='uid' placeholder='username'/>
-                                    <input type='password' name='pwd' placeholder='password'/>
-                                    <button>login</button>
-                                    <p class='message'>Not registered? <a href='signup.php'>Create an account</a></p>
-                                  </form>
-                                </div>
-                              </div> ";
+                               
+                               
                               
                                     
                          }
