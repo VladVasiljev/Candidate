ae176a7e3b178bb8b556f5411f4feec288fae0c3
Vlad Vasiljev <vlad.vasiljevs97@gmail.com>
2017-11-16 12:40:29 +0000

    Deleted un needed files

--- class.user.php
+++ class.user.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once('dbconfig.php');
+require_once('configs/dbconfig.php');
 
 class USER
 {	
--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-$databaseHost = 'localhost';
-$databaseName = 'logintest';
-$databaseUsername = 'root';
-$databasePassword = '';
-
-$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
-?>
