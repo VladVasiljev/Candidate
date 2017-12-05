de194453010aca994a5d40acf26dc4b052854021
Vlad Vasiljev <vlad.vasiljevs97@gmail.com>
2017-12-05 11:51:12 +0000

    Changes to the user profile card information

--- userprofile.php
+++ userprofile.php
@@ -1,4 +1,5 @@
-<?php include 'header.php';?>
+<?php include 'header.php';
+include_once("configs/dbh.php");?>
 <?php
 
 	require_once("session.php");
@@ -25,9 +26,11 @@
 
 <body>
 
+
+
   <!--  <li><a href="logout.php?logout=true"><span class="glyphicon glyphicon-log-out"></span>&nbsp;Sign Out</a></li>-->
 
-
+	
 	<div class="wrapper">
 		<div class="row">
 			<div class="col-1-1">
@@ -37,8 +40,9 @@
 			</div>
 			<div align="center" class="col-1-2">
 			<div class="user_details">
-
+			<img src="user_images/<?php echo $userRow['userPic']; ?>" class="img-rounded" width="200px" height="200px" />
 			<?php
+			/*
 			include_once 'configs/dbconfig.php';
 			$stmt = $auth_user->runQuery("SELECT id,userPic FROM newuser WHERE id = '" . $userRow['id'] . "'");
 			$stmt->execute();
@@ -76,11 +80,37 @@
 				</div>
 				<?php
 			}
-			
+			*/
 		?>
-		
 		<h2>Candidate Details</h2>
+		<div class="UserInformation">
+		<b>First Name: </b><p class="userInformation"><?php echo $userRow['first'];?></p>
+		<br>
+		<b>Last Name: </b><p class="userInformation"><?php echo $userRow['last'];?></p>
+		<br>
+		<b>Username: </b><p class="userInformation"><?php echo $userRow['uid'];?></p>
+		<br>
+		<b>Email Address: </b><p class="userInformation"><?php echo $userRow['email'];?></p>
+		<br>
+		<b>Years of Experince: </b><p class="userInformation"><?php echo $userRow['years'];?></p>
+		<br>
+		<b>Industry Type: </b><p class="userInformation"><?php echo $userRow['industry'];?></p>
+		<br>
+		<b>Biography: </b><p class="userInformation"><?php echo $userRow['bio'];?></p>
+		<br>
+		<b>Account Created On: </b><p class="userInformation"><?php echo $userRow['timestamp'];?></p>
+		<br>
+		<?php $today = new DateTime();
+			$acountCreatedOn = new DateTime($userRow['timestamp']);
+			$interval = $today->diff($acountCreatedOn);
+			echo $interval->format('Your Account is %d Days Old'); ?>
+			</div>
+		 <form action="user_cv/<?php echo $userRow['user_cv']?>"target=_blank>
+        <button class="viewUserCvUserAccount">View CV</button><br><br>
+    </form>	
+		
 		<?php
+		/*
 		include_once 'configs/dbh.php';
 				if (isset($userRow['id'])) {
 				    echo "<h3>Hello</h3>" . $userRow['uid'] ." your profile ID is:";
@@ -94,7 +124,7 @@
 				    if (mysqli_num_rows($result) > 0) {
 				        // output data of each row
 				        while ($row = mysqli_fetch_assoc($result)) {
-							echo  " <br><label>CV:</label> " . $row['user_cv'] . " <br><b>Username:</b> " . $row['uid'] . " <br><b>Name:</b> " . $row["first"] . " " . $row["last"] . " <br><b>Email:</b> " . $row["email"] . "<br><b> Years Experience:</b> " . $row["years"] . "<br> <b>Industry:</b> " . $row["industry"] . " <br>You joined on ".$row['timestamp']."<br><b>Bio:</b> " . $row["bio"];
+							echo  " <br><label>CV:</label> " . $userRow['user_cv'] . " <br><b>Username:</b> " . $userRowrow['uid'] . " <br><b>Name:</b> " . $row["first"] . " " . $row["last"] . " <br><b>Email:</b> " . $row["email"] . "<br><b> Years Experience:</b> " . $row["years"] . "<br> <b>Industry:</b> " . $row["industry"] . " <br>You joined on ".$row['timestamp']."<br><b>Bio:</b> " . $row["bio"];
 							//echo "</br><a href=\"editProfile.php?id=$row[id]\">Edit Profile</a>";
 							echo"<a class='nav-item nav-link' href='logout.php?logout=true'>Sign Out</a>";
 							
@@ -111,23 +141,26 @@
 				} else {
 				    echo "You are not logged in";
 				}
-
+*/
 				?>
 
 
 
 				</div>
 				</div>	 
-				<div class="col-1-2">
-			<!--	<center><h3><?php echo $userRow['uid']?>'s CV</h3></center>
-					<iframe src="user_cv/<?php echo $userRow['user_cv']?>" height=100% width=100%> </iframe>-->
+				<!--
+				<div align="center" class="col-1-2">
+					<div class="userCvImage">
+				<center><h3><?php echo $userRow['uid']?>'s CV</h3></center>
+					<iframe src="user_cv/<?php echo $userRow['user_cv']?>" height=100% width=100%> </iframe>
 				<center><h5>Click here to view <?php echo $userRow['uid']?>'s CV</h5>
 					<a href= "user_cv/<?php echo $userRow['user_cv']?>" target=_blank><img src="img/sample_cv.PNG" width="100%" height="100%"/></a></center>
 				</div>
+			</div>
+			-->
 		</div><!-- /.row -->
-		
+		<?php include 'footer.php'?>
 	</div><!-- /.wrapper -->
-	<?php include 'footer.php'?>
-
+	
 </body>
 </html>
