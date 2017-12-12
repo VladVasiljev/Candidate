7e4abceb4a62327e323f99e21f90bce528602e3f
Vlad Vasiljev <vlad.vasiljevs97@gmail.com>
2017-12-12 16:41:01 +0000

    Redone edit profile styling

--- userprofile.php
+++ userprofile.php
@@ -26,22 +26,120 @@ include_once("configs/dbh.php");?>
 
 <body>
 
+<form method="" enctype="multipart/form-data" class="form-horizontal" role="form" id="userProfileForm">
+	<div class="container">
+    <h3>Hello <?php echo $userRow['first']; ?> <?php echo $userRow['last'];?> Welcome To Your Profile Page</h3>
+  	<hr>
+	<div class="row">
+      <!-- left column -->
+      <div class="col-1-2">
+        <div class="text-center">
+		<!--user image display -->
+		<img src="user_images/<?php echo $userRow['userPic']; ?>" height="250" width="250" class="rounded-circle" alt="avatar" />
+		  <!-- <input class="btn-primary" type="file" name="user_image" class="form-control" accept="image/*">
+		  <p><b>Image upload</b></p>
+		  <input class="btn-primary" type="file" name="user_cv" class="form-control" accept="file_extention/*">
+		  <p><b>CV upload</b></p>
+		  -->
+        </div>
+		<div class="editview">
+		<a  href="editProfile.php?id=<?php echo $userRow['id']; ?>" title="click for edit"> <img id="edit" src="img/edit.png">Edit Profile</a>
+		<a  href="user_cv/<?php echo $userRow['user_cv']; ?>" title="Click To View CV"> <img id="edit" src="img/edit.png">View CV</a>
+	  </div>
+	  </div>
+
+	  
+	  <div class="col-1-2">
+        <div class="alert alert-info alert-dismissable">
+          <a class="panel-close close" data-dismiss="alert">×</a> 
+          <!-- <i class="fa fa-coffee"></i> --> 
+          Please Fill In <strong>All Fields</strong> Before Saving Changes.
+        </div>
+		
+		<h3>Personal information</h3>
+	
+    <?php
+	if(isset($errMSG)){
+		?>
+        <div>
+          <span class="glyphicon glyphicon-info-sign"></span> &nbsp; <?php echo $errMSG; ?>
+        </div>
+        <?php
+	}
+	?>
+
+   
+    
+		<div class="form-group">
+            <label class="col-lg-3 control-label">First Name:</label>
+            <div class="col-lg-8">
+			<input class="form-control" type="text" name="first_name" readonly value="<?php echo $userRow['first']; ?>" />
+            </div>
+		</div>
+		
+		<div class="form-group">
+            <label class="col-lg-3 control-label">Last Name:</label>
+            <div class="col-lg-8">
+			<input class="form-control" type="text" name="last_name" readonly value="<?php echo $userRow['last']; ?>"  />
+            </div>
+		</div>
+		
+		<div class="form-group">
+            <label class="col-lg-3 control-label">Email Address:</label>
+            <div class="col-lg-8">
+			<input class="form-control" type="text" name="email_Address" readonly value="<?php echo $userRow['email']; ?>"  />
+            </div>
+		</div>
+		
+		<div class="form-group">
+            <label class="col-lg-3 control-label">Years of Experience:</label>
+            <div class="col-lg-8">
+			<input class="form-control" type="text" name="years_XP" readonly value="<?php echo $userRow['years']; ?>"  />
+            </div>
+		</div>
+
+		<div class="form-group">
+		<label class="col-lg-3 control-label">Your Industry:</label>
+		<div class="col-lg-8">
+		<input class="form-control" type="text" name="biography" readonly value="<?php echo $userRow['industry']; ?>"  />
+		</div>
+
+		  <div class="form-group">
+		  <label class="col-lg-3 control-label">Your Location:</label>
+		  <div class="col-lg-8">
+		  <input class="form-control" type="text" name="biography" readonly value="<?php echo $userRow['location']; ?>"  />
+		  </div>
+
+		<div class="form-group">
+            <label class="col-lg-3 control-label">Biography:</label>
+            <div class="col-lg-8">
+			<input class="form-control" type="text" name="biography" readonly value="<?php echo $userRow['bio']; ?>"  />
+
+            </div>
+        </div>
+		  </div>
+        </div>
+		</div>
+
+</form>
 
 
   <!--  <li><a href="logout.php?logout=true"><span class="glyphicon glyphicon-log-out"></span>&nbsp;Sign Out</a></li>-->
 
-	
+	<!--
 	<div class="wrapper">
 		<div class="row">
 			<div class="col-1-1">
-			<!--	<center>
+				<center>
 					<img alt="Logo" src="img/logo.png">
-				</center>-->
+				</center>
 			</div>
 			<div align="center" class="col-1-1">
 			<div class="user_details">
-			<img src="user_images/<?php echo $userRow['userPic']; ?>" class="img-rounded" width="200px" height="200px" />
+			<img src="user_images/<?php echo $userRow['userPic']; ?>" class="img-rounded" width="200px" height="200px" /> -->
+		
 			<?php
+
 			/*
 			include_once 'configs/dbconfig.php';
 			$stmt = $auth_user->runQuery("SELECT id,userPic FROM newuser WHERE id = '" . $userRow['id'] . "'");
@@ -82,6 +180,7 @@ include_once("configs/dbh.php");?>
 			}
 			*/
 		?>
+		<!--
 		<h2>Candidate Details</h2>
 		<div class="UserInformation">
 		<a  href="editProfile.php?id=<?php echo $userRow['id']; ?>" title="click for edit"> <img id="edit" src="img/edit.png"></a>
@@ -106,7 +205,7 @@ include_once("configs/dbh.php");?>
 			$interval = $today->diff($acountCreatedOn);
 			echo $interval->format('Your Account is %d Days Old'); ?>
 			</div>
-		 <form action="user_cv/<?php echo $userRow['user_cv']?>"target=_blank>
+		<form action="user_cv/<?php echo $userRow['user_cv']?>"target=_blank>
         <button class="viewUserCvUserAccount">View CV</button><br><br>
     </form>	
 		
